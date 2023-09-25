fe312814f2513660d9b061c5801cc0cabfb42046
Sergii79 <tsymbal.sergiy@gmail.com>
2023-09-25 17:18:17 +0100

    commit first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { RecipeList } from './RecipeList/RecipeList';
+import recipes from '../recipes.json';
+// import recipes from '../recipes.json';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <div>Recipe Form</div>
+      <RecipeList items={recipes} />
+      <div>Image modal</div>
+      <GlobalStyle />
+    </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-import './index.css';
+import { StyleSheetManager } from 'styled-components';
+
+const theme = {
+  colors: {
+    black: '#212121',
+    white: '#fff',
+    accent: 'orangered',
+  },
+  radii: {
+    xs: '2px',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StyleSheetManager shouldForwardProp={prop => prop !== 'active'}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StyleSheetManager>
   </React.StrictMode>
 );
